7e840c314cb32448aba499068867baa29d61a878
mwdziak <mwdziak@gmail.com>
2025-04-10 17:46:26 +0200

    Added INITIAL_SCORE constant to the Scoreboard interface

--- src/main/java/com/example/sportradar/api/Scoreboard.java
+++ src/main/java/com/example/sportradar/api/Scoreboard.java
@@ -3,6 +3,7 @@ package com.example.sportradar.api;
 import java.util.List;
 
 public interface Scoreboard {
+    int INITIAL_SCORE = 0;
     void startMatch(String homeTeam, String awayTeam);
     void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore);
     void endMatch(String homeTeam, String awayTeam);
